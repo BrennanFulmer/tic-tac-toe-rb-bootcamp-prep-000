61be37f2f6b65731316ce834b7b5441d35ae062d
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-11 20:58:13 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,8 +38,6 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    # letter = current_player(board)
-    # move(board, index, letter)
     move(board, index, current_player(board))
     display_board(board)
   else
